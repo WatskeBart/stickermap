b294320c216740a984ad02957fdbc88cc0a7ba7f
WatskeBart <bartistic@gmail.com>
2026-05-16 21:36:57 +0200

    Add map type toggle (street, satellite, terrain) Fixes #62

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -2,7 +2,11 @@ import 'angular-server-side-configuration/process';
 
 export const environment = {
   production: false,
-  tileLayerUrl: process.env.TILESERVER_URL || 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+  tileLayers: {
+    street: process.env.TILESERVER_URL_STREET || 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    satellite: process.env.TILESERVER_URL_SATELLITE || 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    terrain: process.env.TILESERVER_URL_TERRAIN || 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}',
+  },
   keycloak: {
     url: process.env.KEYCLOAK_URL || 'http://localhost:8080',
     realm: process.env.KEYCLOAK_REALM || 'stickermap',
